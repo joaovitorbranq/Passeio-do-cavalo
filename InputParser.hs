00c792d8fc983bc473bb6ce73400c552eb46ba06
joaovitorbranq <joaobranquinhoribeiro@gmail.com>
2025-11-09 20:43:38 -0300

    adiciona funçao para parsear a linha lida no txt (todo).

--- InputParser.hs
+++ InputParser.hs
@@ -21,8 +21,36 @@ trim s = trimRight (trimLeft s)
 
 
 -- converte string para int de forma segura
-readIntSafe :: String -> Maybe Int
-readIntSafe w =
+strToInt :: String -> Maybe Int
+strToInt w =
   case reads w of
     [(x, "")] -> Just x
     _         -> Nothing
+
+
+-- usa lib 'words' para quebrar linha por espaços
+splitWords :: String -> [String]
+splitWords = words
+
+
+-- lê UMA linha no formato:
+-- <linhas> <colunas> <linhaInicial> <colunaInicial>
+parseLine :: String -> Maybe (Int, Int, Pos)
+parseLine s =
+  let ws = splitWords s
+  in case ws of 
+       [a,b,c,d] -> -- exige exatamente 4 itens de splitWords, senão Nothing
+         case strToInt a of
+           Nothing -> Nothing
+           Just rows ->
+             case strToInt b of
+               Nothing -> Nothing
+               Just cols ->
+                 case strToInt c of
+                   Nothing -> Nothing
+                   Just sr ->
+                     case strToInt d of
+                       Nothing -> Nothing
+                       Just sc ->
+                         Just (rows, cols, (sr - 1, sc - 1)) -- converte para 0-based para Pos
+       _ -> Nothing
